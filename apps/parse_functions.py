a340e6e80dcea468862ded4b531acece43d447af
vsem-azamat <almazbek1608@gmail.com>
2023-06-01 15:26:34 +0200

    added bad_word function

--- apps/parse_functions.py
+++ apps/parse_functions.py
@@ -1,6 +1,6 @@
 import requests
 from datetime import datetime
-from typing import List, Tuple, Generator
+from typing import Tuple, Generator
 
 from bs4 import BeautifulSoup
 
@@ -26,3 +26,14 @@ async def get_values_ntk_visits(start_datetime: datetime, end_datetime: datetime
         return x_dates, y_values, list(XY)
     except ValueError:
         return [], [], []
+
+
+async def get_bad_word() -> str:
+    params = {
+        'lang': 'ru'
+        }
+    url = 'https://evilinsult.com/generate_insult.php'
+    response = requests.get(url, params=params)
+    soup = BeautifulSoup(response.text, 'lxml')
+    text = soup.find('body').text
+    return text
--- hanlders.py
+++ hanlders.py
@@ -1,12 +1,12 @@
 import io
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from aiogram import types
 from aiogram.dispatcher.filters import Command
 
 from bot import dp, bot
 from configs import NtkGroup, SuperAdmins
-from apps.parse_functions import get_ntk_quantity
+from apps.parse_functions import get_ntk_quantity, get_bad_word
 from apps.plot_functions import pltGraph
 from apps.predictModels import predictModels
 
@@ -26,16 +26,24 @@ async def ntk(msg: types.Message):
 async def ask_ntk(msg: types.Message):
     """Send help message"""
     text = \
-    "<b>Хай, я создан для чата @chat_ntk!</b>\n\n"\
-    "Команды:\n"\
-    "/ntk - Показать кол-во людей в NTK\n\n"\
-    "admin: t.me/vsem_azamat\n"\
-    "GitHub: github.com/vsem-azamat/ntk_bot/"
-    await msg.answer(text, disable_web_page_preview=True)
+    "🤖<b>Хай, я создан для чата @chat_ntk!</b>\n\n"\
+    "📋<b>Команды:</b>\n"\
+    "/ntk - Показать кол-во людей в NTK\n"\
+    "/graph - Показать график посещений NTK\n"\
+    "/badword - Нужно переслать на чужое сообщение, чтобы оскорбить.\n"
+    keyboard = types.InlineKeyboardMarkup()
+    button_chat = types.InlineKeyboardButton(text='📚NTK chat', url='https://t.me/chat_ntk')
+    buttons = [
+        types.InlineKeyboardButton(text='👨‍🎓Admin', url='t.me/vsem_azamat'),
+        types.InlineKeyboardButton(text='🧑‍💻GitHub', url='github.com/vsem-azamat/ntk_bot/')
+        ]
+    keyboard.add(button_chat)
+    keyboard.add(*buttons)
+    await msg.answer(text, reply_markup=keyboard, disable_web_page_preview=True)
     await msg.delete()
 
 
-@dp.message_handler(Command('graph', prefixes='!/'), SuperAdmins())
+@dp.message_handler(Command('graph', prefixes='!/'), NtkGroup())
 async def send_stats(msg: types.Message):
     """Send graph with prediction"""
     fig_predict = await pltGraph.daily_graph_with_predictions()
@@ -47,6 +55,7 @@ async def send_stats(msg: types.Message):
         photo=types.InputFile(image_buffer),
         caption=str(datetime.now().strftime('%d-%m-%Y'))
     )
+    await msg.delete()
 
 
 @dp.message_handler(Command('learn', prefixes='!/'), SuperAdmins())
@@ -54,4 +63,12 @@ async def learn_models(msg: types.Message):
     """Learn models"""
     await predictModels.learn_models()
     await msg.answer('Models learned!')
+    await msg.delete()
+
+
+@dp.message_handler(Command('badword', prefixes='!/'), NtkGroup())
+async def send_bad_word(msg: types.Message):
+    if msg.reply_to_message:
+        text = await get_bad_word()
+        await msg.reply_to_message.reply(text)
     await msg.delete()
